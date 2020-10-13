f14a721ace134ce7d292c555ba754f8bb58d6f51
AKen00 <ajken04@gmail.com>
2020-10-13 11:26:41 -0400

    new list

--- index.js
+++
@@ -1,8 +0,0 @@
-const Team = require("./Team");
-const inquirer = require("inquirer");
-
-// Initalize a new Team object. 
-const team = new team();
-
-// Start gnerating the Team Profile. 
-team.start();
